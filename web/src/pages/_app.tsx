1c83d2c8cc306c77025414a562366701b84f62bc
Doringl <49004028+Doringl@users.noreply.github.com>
2021-03-03 16:40:50 +0300

    adding favicon and adding main page title ...

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,14 +1,38 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import theme from '../theme';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <title>Wikipetty</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
